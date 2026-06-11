8f2e870e85efa43597d12adaa61c44e290bfe030
roboticsubspa-gif <roboticsubspa@gmail.com>
2026-06-11 01:32:56 -0400

    Remove empty line at the beginning of App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './contexts/AuthContext';
 import { Layout } from './components/Layout';
